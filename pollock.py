c93aaa34e5df00cad5e8f34c7024ce8f84a310cf
Andrew Blankfield <blankfieldatwork@gmail.com>
2015-08-23 21:12:15 -0400

    close to finished

--- pollock.py
+++ pollock.py
@@ -19,42 +19,47 @@ class Pollock:
 
         curses.start_color()
         curses.use_default_colors()
-        self.counti = 0
-        self.countx = 0
+        self.count = 0
         for i in range(0,curses.COLORS):
-            curses.init_pair(i+1,i, -1)
+            curses.init_pair(i+1,-1, i)
             for x in range(0,curses.COLORS):
-                if x != i:
-                    curses.init_pair(i+x+1, i, x)
+                if x == i:
+                    continue
+                curses.init_pair(i+x+1, i, x)
 
     def add_footer(self,msg):
         message_length = len(msg)
         width = self.board.width
         position = int((width - message_length) / 2)
+        current = " {} of {} complete, or {}% ".format(self.count -1,
+                self.number_of_strokes, int(self.count/self.number_of_strokes * 100))
+        cpos = int((width - len(current)) /2)
         self.screen.addstr(self.board.height-2, position,"[{}]".format(msg))
+        self.screen.addstr(self.board.height-1, cpos, "[{}]".format(current))
         self.screen.refresh()
 
     def get_random_spots(self,num):
         return random.sample(self._spots, num)
 
     def stroke(self):
-        num_spots = random.randint(0,int(len(self._spots)/18))
+        num_spots = random.randint(0,int(len(self._spots)*.008))
         spots = self.get_random_spots(num_spots)
         char = random.choice(self.chars)
-        color = random.choice(range(0,31))
+        color = random.choice(range(0,random.choice(range(8,24))))
         for spot in spots:
             y,x = spot
             self.screen.addstr(y,x,char,curses.color_pair(color))
 
     def start(self):
         self.board.start()
-        count = 0
-        while count <= 10020:
+        self.number_of_strokes = random.randint(5,1337)
+        while self.count <= self.number_of_strokes:
+            self.count += 1
             self.stroke()
-            count += 1
-            time.sleep(.1)
-            self.add_footer("-Shhh... Pollock is painting-")
+            self.add_footer("-Shhh... Pollock is painting -")
             self.screen.refresh()
+            time.sleep(.1)
+        self.add_footer("-Ah! He is done! Beautiful!-")
         time.sleep(10)
         self.board.stop()
 
